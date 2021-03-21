501cefb88fe6adb92aa22b9678cc45c6a5ea97cd
Виктория Власова <viktoriya_vlasova_00@list.ru>
2021-03-21 17:24:24 +0700

    Создан проект бизнес-логики NoteApp с добавлением классов и логики

---
+++ NoteApp/NoteApp/Note.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteApp
+{
+    public class Note
+
+    {
+
+        private string _name;
+        private Category _category;
+        private string _textNote;
+        private DateTime _dateCreate;
+        private DateTime _dateModific;
+
+        public Category Category
+        {
+            get { return _category; }
+            set
+            {
+                switch (value)
+                {
+                    case Category.Work:
+                        {
+                            _category = Category.Work;
+                            break;
+                        }
+                    case Category.Home:
+                        {
+                            _category = Category.Home;
+                            break;
+                        }
+                    case NoteApp.Category.HealthAndSport:
+                        {
+                            _category = NoteApp.Category.HealthAndSport;
+                            break;
+                        }
+                    case Category.People:
+                        {
+                            _category = Category.People;
+                            break;
+                        }
+                    case Category.Documents:
+                        {
+                            _category = Category.Documents;
+                            break;
+                        }
+                    case Category.Finance:
+                        {
+                            _category = Category.Finance;
+                            break;
+                        }
+                    case Category.Different:
+                        {
+                            _category = Category.Different;
+                            break;
+                        }
+                    default:
+
+                        break;
+                }
+
+            }
+        }
+
+        /// <summary>
+        /// Свойство назнавия заметки.
+        /// </summary>
+        public string Name
+        {
+            get { return _name; }
+
+            set
+            {
+
+                if (value.Length < 50)
+
+                {
+                    throw new ArgumentException("Количество символов в заметке не может превышать 50");
+                }
+                else if (value.Length != 0)
+                {
+                    throw new ArgumentException("Количество символов в заметке не может быть нулевым");
+                }
+
+                else
+                {
+                    _name = value;
+                }
+
+            }
+        }
+
+        public string TextNote
+        {
+            get { return _textNote; }
+
+            set { _textNote = value; }
+
+        }
+
+        public DateTime DateCreate
+        {
+
+            get { return _dateCreate; }
+
+        }
+
+
+        public DateTime DateModific
+        {
+            get { return _dateModific; }
+
+            set
+            {
+                _dateModific = DateTime.Now;
+
+            }
+
+        }
+
+    }
+
+    
+
+}
---
+++ NoteApp/NoteApp/ProjectManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+namespace NoteApp
+{
+    static public class ProjectManager
+    {
+        private static string _pathfile = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\NoteApp\\NoteApp.json";
+
+        public static string DefaultFileName
+        {
+            get
+            {
+                return _pathfile;
+            }
+        }
+
+        public static void SaveDate(Project project, string nameFile)
+        {
+            //Создаём экземпляр сериализатора
+            JsonSerializer serializer = new JsonSerializer();
+            //Открываем поток для записи в файл с указанием пути
+            using (StreamWriter sw = new StreamWriter(nameFile))
+            using (JsonWriter writer = new JsonTextWriter(sw))
+            {
+                //Вызываем сериализацию и передаем объект, который хотим сериализовать
+                serializer.Serialize(writer, project);
+            }
+        }
+        public static Project LoadDate(string nameFile)
+        {
+            Project project = null;
+            //Создаём экземпляр сериализатора
+            JsonSerializer serializer = new JsonSerializer();
+            //Открываем поток для чтения из файла с указанием пути
+            using (StreamReader sr = new StreamReader(nameFile))
+            using (JsonReader reader = new JsonTextReader(sr))
+            {
+                //Вызываем десериализацию и явно преобразуем результат в целевой тип данных
+                project = (Project)serializer.Deserialize<Project>(reader);
+            }
+            
+            return project;
+
+        }
+    }
+   
+}
